6e2405df87375042da89ffd0e2c93584184be263
lemonstar99 <44111674+lemonstar99@users.noreply.github.com>
2021-05-11 00:38:08 +0900

    Update 1463.cpp
    cin cout는 코드에서만 봤지 컴파일 될때 어떻게 되는지 원리 같은걸 안 봤더니, 이 방법이 시간이 오래 걸리는줄 몰랐다.

--- 2021 05 May/1463.cpp
+++ 2021 05 May/1463.cpp
@@ -1,42 +1,47 @@
-// 시간초과
+// 시간초과 문제 해결 방법: cin cout은 입출력이 많을 때 시간이 많이 걸린다!
+
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
-int search(int* A, int num, int low, int high){
-    int mid;
+void search(int* A, int n, int num){
+    int low = 0;
+    int high = n - 1;
+    int mid = (low + high) / 2;
     
     while(low <= high){
         mid = (low + high) / 2;
         if (num == A[mid]){
-            return 1;
+            printf("1\n");
+            break;
         } else if (num < A[mid]){
             high = mid - 1;
         } else {
             low = mid + 1;
         }
     }
-    return 0;
+    
+    if (low > high && A[mid] != num){
+        printf("0\n");
+    }
 }
 
 int main(){
-    int i, n, m, input, answer = 0;
-    cin >> n;
+    int i, n, m, input = 0;
+    scanf("%d", &n);
     
     int A[n];
     for (i = 0; i < n; i++){
-        cin >> A[i];
+        scanf("%d", &A[i]);
     }
     
     sort(A, A + n);
-    int* B = A;
     
     cin >> m;
     for (i = 0; i < m; i++){
-        cin >> input;
-        answer = search(B, input, 0, n);
-        cout << answer << "\n";
+        scanf("%d", &input);
+        search(A, n, input);
     }
     
     return 0;
