437472dfa801d040b4d4a9f580c596a4ddc0d4e4
서다빈 Dabin Seo <44111674+sodabeans@users.noreply.github.com>
2022-03-19 21:50:45 +0900

    Dynamic Programming 9095
    Adding 1s, 2s, and 3s

---
+++ Baekjoon/2022 03 March/9095.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+dp = [0, 1, 2, 4] + [0] * 8
+
+for i in range(4, 12):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+for _ in range(T):
+    num = int(input())
+    print(dp[num])
