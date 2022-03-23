ebfb260c18d4c7116e9b079f7fab9d1622443fbd
서다빈 Dabin Seo <44111674+sodabeans@users.noreply.github.com>
2022-03-23 20:14:42 +0900

    Update Graph Search 11725
    Using a different method to resolve time-out

--- Baekjoon/2022 03 March/11725.py
+++ Baekjoon/2022 03 March/11725.py
@@ -1,18 +1,32 @@
-"""
-# 시간 초과
-
+from collections import deque
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 tree = [[] for _ in range(N + 1)]
-parent = [0] * (N + 1)
+answer = [0 for _ in range(N + 1)]
 
 # connect nodes to each other, unrooted
 for _ in range(N - 1):
     a, b = map(int, input().split())
     tree[a].append(b)
     tree[b].append(a)
+
+nodes = deque()
+nodes.append(1)
+
+while nodes:
+    parent = nodes.popleft()
+    for p in tree[parent]:
+        if answer[p] == 0: # initialized as 0, so 0 is not visited
+            answer[p] = parent
+            nodes.append(p)
+
+for i in range(2, N + 1):
+    print(answer[i])
+
+"""
+# 시간 초과
 
 # check parent node starting from 1
 visited = [1]
@@ -23,7 +37,4 @@ while(len(visited) < N):
             if t not in visited:
                 visited.append(t)
                 parent[t] = node
-
-for i in range(2, N + 1):
-    print(parent[i])
 """
