3f43a5426f6cc2aef55eb54939d678facc2a4c28
서다빈 Dabin Seo <44111674+sodabeans@users.noreply.github.com>
2022-04-05 09:34:38 +0900

    Dynamic Programming 12852
    Make into 1 2

--- Baekjoon/12852.py
+++ Baekjoon/12852.py
@@ -1,15 +1,26 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
+N = int(input())
 
 dp = [0] * (N + 1)
+ans = [[0]] * (N + 1)
+ans[1] = [1]
 
 for i in range(2, N + 1):
-    dp[i] = dp[i-1] + 1 # case 3
-    if (i % 2 == 0):
-        # error message: "TypeError: list indices must be integers or slices, not float"
-        dp[i] = min(dp[i], dp[i // 2] + 1) # // is integer division
-    if (i % 3 == 0):
+    dp[i] = dp[i - 1] + 1
+    if i % 2 == 0:
+        if dp[i] > dp[i // 2] + 1:
+            ans[i] = ans[i // 2] + [i]
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        if dp[i] > dp[i // 3] + 1:
+            ans[i] = ans[i // 3] + [i]
         dp[i] = min(dp[i], dp[i // 3] + 1)
+    if ans[i] == [0]:
+        ans[i] = ans[i - 1] + [i]
+
+ans[N].sort(reverse=True)
 
 print(dp[N])
+print(*ans[N], sep=' ')
