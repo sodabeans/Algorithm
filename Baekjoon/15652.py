d153b04395fa18f86343bd110ffa64b185b6b6fc
서다빈 Dabin Seo <44111674+sodabeans@users.noreply.github.com>
2022-05-10 23:26:27 +0900

    [DFS, Backtracking] "N과 M (4)" 15652

---
+++ Baekjoon/15652.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+ans = []
+
+
+def dfs():
+    if len(ans) == M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(1, N+1):
+        if len(ans) and ans[-1] > i:
+            continue
+        ans.append(i)
+        dfs()
+        ans.pop()
+
+
+dfs()
