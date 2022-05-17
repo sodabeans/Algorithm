026f69e7b8485317195fad0d603c6938c638e61f
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-05-18 00:11:15 +0900

    [Number Theory, Prime Number] "소인수분해" 11653

--- Baekjoon/11653.py
+++ Baekjoon/11653.py
@@ -2,24 +2,20 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-ans = list()
 
-prime = [True for _ in range(1000001)]
+prime = [True for _ in range(10000001)]
 
 p = 2
-while (p ** 2 <= 1000000):
-    if prime[p] == True:
-        for i in range(p ** 2, 1000001, p):
+while p ** 2 <= 10000000:
+    if prime[p]:
+        for i in range(p ** 2, 10000001, p):
             prime[i] = False
     p += 1
 
-while N > 1:
-    for i in range(2, len(prime)):
-        if prime[i] and N % i == 0:
-            ans.append(i)
-            N = N / i
-            continue
-
-ans.sort()
-for a in ans:
-    print(a)
+i = 2
+while N > 1 or i < 10000001:
+    if prime[i] and N % i == 0:
+        print(i)
+        N = N / i
+        continue
+    i += 1
