1803b4d84d9336b5626d907ed1cdec57a28b52d4
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-05-29 16:45:43 +0900

    [Brute Force, Backtracking] "눈덩이 굴리기" 21735 (Update)

--- Baekjoon/21735.py
+++ Baekjoon/21735.py
@@ -13,9 +13,9 @@ def dfs(idx, snowball, time):
     else:
         ans = max(snowball, ans)
 
-    if idx < N - 1:
+    if idx <= N - 1:
         dfs(idx + 1, snowball + a[idx + 1], time + 1)
-    if idx < N - 2:
+    if idx <= N - 2:
         dfs(idx + 2, snowball // 2 + a[idx + 2], time + 1)
 
 
