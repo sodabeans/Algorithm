021a9fc182a497ea16d38ad0dc025361d487fcca
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-06-15 23:41:56 +0900

    [Priority Queue, Tree, Set, Map] "이중 우선순위 큐 Dual PQ" 7662 (Update)

--- Baekjoon/7662.py
+++ Baekjoon/7662.py
@@ -1,25 +1,44 @@
-from collections import deque
+import heapq
 import sys
 input = sys.stdin.readline
 
 T = int(input())
 
 for _ in range(T):
-    entries = deque()
+    heap_max = []
+    heap_min = []
     k = int(input())
-    for _ in range(k):
+    visited = [False] * k
+
+    for idx in range(k):
         calc, n = map(str, input().split())
         n = int(n)
-        if calc == 'I':
-            entries.append(n)
-        else:
-            if entries:
-                entries = deque(sorted(entries))
-                if n == 1:
-                    entries.pop()
-                else:
-                    entries.popleft()
-    if entries:
-        print(f'{entries[-1]} {entries[0]}')
+        if calc == 'I':  # insert
+            # group input and index together
+            heapq.heappush(heap_max, [-n, idx])
+            heapq.heappush(heap_min, [n, idx])
+            visited[idx] = True
+        else:  # delete
+            if n == 1:  # delete max
+                while heap_max and not visited[heap_max[0][1]]:
+                    # element exists and element was not visited
+                    heapq.heappop(heap_max)
+                if heap_max:
+                    visited[heap_max[0][1]] = False
+                    heapq.heappop(heap_max)
+            else:  # delete min
+                while heap_min and not visited[heap_min[0][1]]:
+                    heapq.heappop(heap_min)
+                if heap_min:
+                    visited[heap_min[0][1]] = False
+                    heapq.heappop(heap_min)
+
+    while heap_max and not visited[heap_max[0][1]]:
+        heapq.heappop(heap_max)
+    while heap_min and not visited[heap_min[0][1]]:
+        heapq.heappop(heap_min)
+
+    if not heap_max and not heap_min:
+        print("EMPTY")
     else:
-        print('EMPTY')
+        print(f'{-heap_max[0][0]} {heap_min[0][0]}')
