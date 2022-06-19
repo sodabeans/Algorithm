4280f84b90ca680863ae93781e266963052f3fb5
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-06-19 21:48:31 +0900

    [Graph Search, BFS, DFS] "유기농 배추" 1012 (Update)

--- Baekjoon/1012.py
+++ Baekjoon/1012.py
@@ -4,27 +4,40 @@ input = sys.stdin.readline
 
 directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 
+
+def bfs(field, Y, X):
+    queue = deque()
+    queue.append((X, Y))
+    field[Y][X] = 0
+    while queue:
+        x, y = queue.popleft()
+        field[y][x] = 0
+        for i in range(4):
+            dx, dy = directions[i]
+            new_x = x + dx
+            new_y = y + dy
+            if 0 <= new_x < M and 0 <= new_y < N and field[new_y][new_x] == 1:
+                field[new_y][new_x] = 0
+                queue.append((new_x, new_y))
+
+
 T = int(input())
 
 for _ in range(T):
     M, N, K = map(int, input().split())
     field = [[0 for _ in range(M)] for _ in range(N)]
-    queue = deque()
+    cabbage = deque()
+    cnt = 0
+
     for _ in range(K):
         X, Y = map(int, input().split())
         field[Y][X] = 1
-        queue.append((X, Y))
-    cnt = 0
-    while queue:
-        x, y = queue.popleft()
-        field[y][x] = 0
-        curr = True
-        for i in range(4):
-            dx, dy = directions[i]
-            if 0 <= x + dx < M and 0 <= y + dy < N:
-                if field[y + dy][x + dx] == 1:
-                    curr = False
-        if curr:
-            cnt += 1
+        cabbage.append((X, Y))
+
+    for i in range(N):
+        for j in range(M):
+            if field[i][j] == 1:
+                bfs(field, i, j)
+                cnt += 1
 
     print(cnt)
