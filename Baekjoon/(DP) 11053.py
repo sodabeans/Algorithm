7582d209fa176229be76cf77e5576784e82555af
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-07-17 22:08:43 +0900

    [Dynamic Programming] "가장 긴 증가하는 부분 수열" (Update)

--- Baekjoon/(DP) 11053.py
+++ Baekjoon/(DP) 11053.py
@@ -3,13 +3,12 @@ input = sys.stdin.readline
 
 N = int(input())
 series = list(map(int, input().split()))
-dp = [0] * (N + 1)
-ans = 1
+dp = [0] * N
 
-for i in range(1, N):
-    if series[i - 1] >= series[i]:
-        series[i] = series[i - 1]
-    else:
-        ans += 1
+for i in range(N):
+    dp[i] = 1
+    for j in range(0, i):
+        if series[i] > series[j] and dp[i] < dp[j] + 1:
+            dp[i] = dp[j] + 1
 
-print(ans)
+print(max(dp))
