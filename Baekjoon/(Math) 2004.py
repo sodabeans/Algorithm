9873c8372c83517a4881c57934673b7f0f9a9c1e
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-09-09 01:09:24 +0900

    [Number Theory] "조합 0의 개수" (Timeout Again)

--- Baekjoon/(Math) 2004.py
+++ Baekjoon/(Math) 2004.py
@@ -3,18 +3,29 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 m = min(m, n - m)
-numerator = 1
-denominator = 1
-for idx in range(1, m + 1):
-    numerator *= n - idx + 1
-    denominator *= idx
-target = numerator // denominator
+cnt_2 = 0
+cnt_5 = 0
 
-ans = 0
-while True:
-    if target % 10 == 0:
-        target = target // 10
-        ans += 1
-    else:
-        print(ans)
-        break
+for idx in range(1, m + 1):
+    upper = n - idx + 1
+    lower = idx
+    while True:
+        if upper % 2 == 0:
+            upper = upper // 2
+            cnt_2 += 1
+        elif upper % 5 == 0:
+            upper = upper // 5
+            cnt_5 += 1
+        else:
+            break
+    while True:
+        if lower % 2 == 0:
+            lower = lower // 2
+            cnt_2 -= 1
+        elif lower % 5 == 0:
+            lower = lower // 5
+            cnt_5 -= 1
+        else:
+            break
+
+print(min(cnt_2, cnt_5))
