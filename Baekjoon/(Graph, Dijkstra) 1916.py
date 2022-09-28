64265244317248d952c218a6fd88d9eb64a839de
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-09-28 23:11:59 +0900

    Dijkstra algorithm using heap (g5)

--- Baekjoon/(Graph, Dijkstra) 1916.py
+++ Baekjoon/(Graph, Dijkstra) 1916.py
@@ -1,26 +1,35 @@
+import heapq
 import sys
 input = sys.stdin.readline
+
 N = int(input())
 M = int(input())
-costs = [[10000000001 for _ in range(N)] for _ in range(N)]
+graph = [[] for _ in range(N + 1)]
+ans = [1000000001 for _ in range(N + 1)]
 
 for _ in range(M):
-    i, j, cost = map(int, input().split())
-    costs[i - 1][j - 1] = cost
-    costs[j - 1][i - 1] = cost
+    tmp_start, tmp_end, tmp_distance = map(int, input().split())
+    graph[tmp_start].append([tmp_end, tmp_distance])
 
 start, end = map(int, input().split())
-start -= 1
-end -= 1
 
-visited = [False for _ in range(N)]
-ans = costs[start].copy()
-visited[start] = True
 
-for _ in range(N):
-    curr = ans.index(min(ans))
-    visited[curr] = True
-    for idx in range(N):
-        if not visited[idx]:
-            ans[idx] = min(costs[curr][idx] + ans[curr], ans[idx])
+def dijkstra(start_node):
+    queue = []
+    heapq.heappush(queue, (0, start_node))
+    ans[start_node] = 0
+    while queue:
+        curr_distance, curr = heapq.heappop(queue)
+        if ans[curr] < curr_distance:
+            continue
+        for next_info in graph[curr]:
+            next_node = next_info[0]
+            next_distance = next_info[1]
+            next_distance += curr_distance
+            if ans[next_node] > next_distance:
+                ans[next_node] = next_distance
+                heapq.heappush(queue, (next_distance, next_node))
+
+
+dijkstra(start)
 print(ans[end])
