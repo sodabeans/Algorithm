ed96984f2288cf899af31185a4ef3d7a08ba6e3c
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-10-05 14:49:10 +0900

    Cumulative Sum (pass with pypy3) s1

--- Baekjoon/(Cumulative Sum) 16139.py
+++ Baekjoon/(Cumulative Sum) 16139.py
@@ -3,11 +3,18 @@ input = sys.stdin.readline
 
 S = input().rstrip()
 q = int(input())
-cnt = [[0 for _ in range(len(S))] for _ in range(26)]
+cnt = [[0] * 26 for _ in range(len(S))]
 
-for idx in range(len(S)):
-    cnt[ord(S[idx]) - 97][idx] += 1
+cnt[0][ord(S[0]) - 97] = 1
+for idx in range(1, len(S)):
+    cnt[idx][ord(S[idx]) - 97] = 1
+    for j in range(26):
+        cnt[idx][j] += cnt[idx - 1][j]
 
 for _ in range(q):
-    a, l, r = map(str, input().split())
-    print(sum(cnt[ord(a) - 97][int(l):int(r)+1]))
+    input_arr = input().split()
+    if int(input_arr[1]) > 0:
+        ans = cnt[int(input_arr[2])][ord(input_arr[0]) - 97] - cnt[int(input_arr[1]) - 1][ord(input_arr[0]) - 97]
+    else:
+        ans = cnt[int(input_arr[2])][ord(input_arr[0]) - 97]
+    print(ans)
