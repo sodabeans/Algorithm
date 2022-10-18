54fdcf614ac27da088b3df92b584119ce81129c6
서다빈 Dabin Seo <44111674+sodabeans@users.noreply.github.com>
2022-10-18 16:47:14 +0900

    (Time Out) Comparing strings and using sets

---
+++ Programmers/Level 3/64064.py
@@ -0,0 +1,24 @@
+# Timeout in 1 case
+import itertools
+
+def solution(user_id, banned_id):
+    answer = 0
+    cases = [[] for _ in range(len(banned_id))]
+    for i in range(len(banned_id)):
+        for idx in range(len(user_id)):
+            if len(banned_id[i]) != len(user_id[idx]):
+                continue
+            flag = True
+            for j in range(len(banned_id[i])):
+                if banned_id[i][j] == '*':
+                    continue
+                if banned_id[i][j] != user_id[idx][j]:
+                    flag = False
+                    break
+            if flag:
+                cases[i].append(idx)                
+    print(cases)
+    possible = set([_ for _ in itertools.product(*cases)])
+    possible = [tuple(set(_)) for _ in possible if len(set(_)) == len(banned_id)]
+    return len(set(possible))
+  
