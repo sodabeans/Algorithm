82607d0447a943d0a0dbdef1411b342ae94b04d1
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-10-23 09:15:48 +0900

    backtracking "N과 M (10)"

---
+++ Baekjoon/(Backtracking) 15664.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+curr = []
+visited = [0] * (N + 1)
+
+
+def selection(depth):
+    if depth == M:
+        print(' '.join(map(str, curr)))
+        return
+    prev = 0
+    for i in range(N):
+        if not visited[i] and prev != nums[i]:
+            if curr and curr[-1] > nums[i]:
+                continue
+            visited[i] = 1
+            prev = nums[i]
+            curr.append(prev)
+            selection(depth + 1)
+            curr.pop()
+            visited[i] = 0
+
+
+selection(0)
