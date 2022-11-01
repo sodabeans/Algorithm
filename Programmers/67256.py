01349827c7429b58493801aada623993f3def112
서다빈 Dabin Seo <44111674+sodabeans@users.noreply.github.com>
2022-11-01 12:44:58 +0900

    distance in a keypad

--- Programmers/67256.py
+++ Programmers/67256.py
@@ -1,28 +1,43 @@
 def solution(numbers, hand):
     answer = ''
-    left = [0, 3]
-    right = [2, 3]
     hand = hand[0].upper()
-    for curr in numbers:
-        print(f'{left} {right}')
-        if curr == 0:
-            curr = 11
-        if curr % 3 == 2:
-            dis_L = (left[0] - 1) ** 2 + (left[1] - (curr + 1) // 3 + 1) ** 2
-            dis_R = (right[0] - 1) ** 2 + (right[1] - (curr + 1) // 3 + 1) ** 2
-            if dis_L == dis_R:
-                answer += hand[0]
-            elif dis_L < dis_R:
-                answer += 'L'
-                left = [1, (curr + 1) // 3 - 1]
-            else:
-                answer += 'R'
-                right = [1, (curr + 1) // 3 - 1]
-        elif curr % 3 == 1:
+    
+    L_row = 3
+    L_col = 0
+    R_row = 3
+    R_col = 2
+    
+    for n in numbers:
+        if n == 0:
+            n = 11
+
+        if n % 3 == 1:  # first column
             answer += 'L'
-            left = [0, ((curr + 2) // 3) - 1]
-        else:
+            L_row = ((n + 2) // 3) - 1
+            L_col = 0
+        elif n % 3 == 0:  # third column
             answer += 'R'
-            right = [2, (curr // 3) - 1]
+            R_row = (n // 3) - 1
+            R_col = 2
+        else:  # second column
+            row = ((n + 1) // 3) - 1
+            dis_L = abs(L_row - row) + abs(L_col - 1)
+            dis_R = abs(R_row - row) + abs(R_col - 1)
+            if dis_L == dis_R:
+                answer += hand
+                if hand == 'L':
+                    L_row = row
+                    L_col = 1
+                else:
+                    R_row = row
+                    R_col = 1
+            elif dis_L > dis_R:
+                answer += 'R'
+                R_row = row
+                R_col = 1
+            else:
+                answer += 'L'
+                L_row = row
+                L_col = 1
+    
     return answer
-  
