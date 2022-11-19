573856c20f093268f502a2874a3acc82c78b2149
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-11-19 11:36:55 +0900

    삽입 정렬 insertion sort

---
+++ ThisIsCodingTest Book/ch06_sorting/06-3.py
@@ -0,0 +1,14 @@
+# insertion sort
+import sys
+input = sys.stdin.readline
+
+arr = list(map(int, input().split()))
+
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+        else:
+            break
+
+print(*arr, sep=' ')
