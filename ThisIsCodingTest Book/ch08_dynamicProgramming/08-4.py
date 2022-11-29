4da2258409d76cfdcc184ca8a7582a647d0147c0
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-11-29 22:29:25 +0900

    피보나치 수열

---
+++ ThisIsCodingTest Book/ch08_dynamicProgramming/08-4.py
@@ -0,0 +1,15 @@
+# fibonacci
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+if N == 1 or N == 2:
+    print(1)
+    exit()
+dp = [0] * (N + 1)
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3, N + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+print(dp[N])
