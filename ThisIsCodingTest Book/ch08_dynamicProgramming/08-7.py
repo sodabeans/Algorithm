448b25e0bdb5407ca829f48927de231c432d8492
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-12-02 21:17:28 +0900

    바닥 공사

---
+++ ThisIsCodingTest Book/ch08_dynamicProgramming/08-7.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * (N + 1)
+dp[1] = 1
+if N == 1:
+    print(dp[N])
+    exit()
+dp[2] = 3
+for i in range(2, N + 1):
+    dp[i] = (dp[i - 2] + 2 * dp[i - 1]) % 796796
+print(dp[N])
