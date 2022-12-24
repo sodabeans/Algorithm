21fc683201e71fe2112db93a48b879d3990f0ec4
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-12-24 16:30:32 +0900

    dynamic programming. find sum with squares

--- Baekjoon/(DP) 1699.py
+++ Baekjoon/(DP) 1699.py
@@ -2,16 +2,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [0] * (N + 1)
+dp = [N] * (N + 1)
 options = []
-for i in range(N):
-    if i ** 2 <= N:
-        options.append(i ** 2)
+for i in range(1, N + 1):
+    if i * i <= N + 1:
+        options.append(i * i)
+    else:
+        break
 
 for i in range(len(options)):
     curr = options[i]
     dp[curr] = 1
     for j in range(curr + 1, N + 1):
-        dp[j] = dp[j - curr] + 1
+        dp[j] = min(dp[j - curr] + 1, dp[j])
 
 print(dp[-1])
