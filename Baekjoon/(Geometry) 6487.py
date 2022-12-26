8f4ca05f43bc4eee259ee5ab8a44840a9da122d7
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-12-26 23:23:54 +0900

    geometry using vectors and matrix

--- Baekjoon/(Geometry) 6487.py
+++ Baekjoon/(Geometry) 6487.py
@@ -5,45 +5,22 @@ N = int(input())
 
 for _ in range(N):
     x1, y1, x2, y2, x3, y3, x4, y4 = map(float, input().split())
+    line1_1 = x2 - x1
+    line1_2 = y2 - y1
+    line1_3 = (x1 * y2) - (x2 * y1)
+    line2_1 = x4 - x3
+    line2_2 = y4 - y3
+    line2_3 = (x3 * y4) - (x4 * y3)
 
-    # NONE special cases
-    if x1 == x2 and y1 != y2 and x3 == x4 and y3 != y4:  # two vertical lines
-        print("NONE")
-        continue
-    if x1 != x2 and y1 == y2 and x3 != x4 and y3 == y4 :  # two horizontal lines
-        print("NONE")
-        continue
+    det = line1_1 * line2_2 - line2_1 * line1_2
 
-    # LINE special cases
-    if x1 == x2 == x3 == x4 or y1 == y2 == y3 == y4:
-        print("LINE")
-        continue
-
-    # POINT special cases
-    if x1 == x2 and y1 != y2:  # only first line is vertical
-        slope = (y4 - y3) / (x4 - x3)
-        y_int = y3 - (slope * x3)
-        ans_y = slope * x1 + y_int
-        print(f'POINT {x1:.2f} {ans_y:.2f}')
-        continue
-    if x3 == x4 and y3 != y4:  # only second line is vertical
-        slope = (y2 - y1) / (x2 - x1)
-        y_int = y1 - (slope * x1)
-        ans_y = slope * x3 + y_int
-        print(f'POINT {x3:.2f} {ans_y:.2f}')
-        continue
-
-    slope_one = (y2 - y1) / (x2 - x1)
-    y_int_one = y1 - (slope_one * x1)
-    slope_two = (y4 - y3) / (x4 - x3)
-    y_int_two = y3 - (slope_two * x3)
-
-    if slope_one == slope_two:
-        if y_int_one == y_int_two:
-            print("LINE")
-        else:
-            print("NONE")
+    if det:
+        x = (line1_1 * line2_3 - line1_3 * line2_1) / det
+        y = (line1_2 * line2_3 - line1_3 * line2_2) / det
+        print(f'POINT {x:.2f} {y:.2f}')
     else:
-        ans_x = (y_int_one - y_int_two) / (slope_two - slope_one)
-        ans_y = (slope_two * y_int_one - slope_one * y_int_two) / (slope_two - slope_one)
-        print(f'POINT {ans_x:.2f} {ans_y:.2f}')
+        check = line1_2 * (x3 - x1) - line1_1 * (y3 - y1)
+        if check:
+            print("NONE")
+        else:
+            print("LINE")
