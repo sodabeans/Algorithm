30700dab2ecd9e4bc4fc624421f4118c65db449a
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-12-27 23:35:17 +0900

    dynamic programming with 2d list (wrong)

--- Baekjoon/(DP) 1890.py
+++ Baekjoon/(DP) 1890.py
@@ -4,27 +4,21 @@ input = sys.stdin.readline
 
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * N for _ in range(N)]
-dp = [[0] * N for _ in range(N)]
+dp = [[0 for _ in range(N)] for _ in range(N)]
 
 queue = deque()
 queue.append((0, 0))
+
 while queue:
-    x, y = queue.popleft()
-    curr = board[x][y]
-    if curr == 0:
+    curr_x, curr_y = queue.popleft()
+    value = board[curr_x][curr_y]
+    if value == 0:
         continue
-    x_new = x + curr
-    y_new = y + curr
-    if x_new < N:
-        dp[x_new][y] += 1
-        if visited[x_new][y] == 0:
-            visited[x_new][y] = 1
-            queue.append((x_new, y))
-    if y_new < N:
-        dp[x][y_new] += 1
-        if visited[x][y_new] == 0:
-            visited[x][y_new] = 1
-            queue.append((x, y_new))
+    if curr_x + value < N and dp[curr_x + value][curr_y] == 0:
+        dp[curr_x + value][curr_y] += dp[curr_x][curr_y] + 1
+        queue.append((curr_x + value, curr_y))
+    if curr_y + value < N and dp[curr_x][curr_y + value] == 0:
+        dp[curr_x][curr_y + value] += dp[curr_x][curr_y] + 1
+        queue.append((curr_x, curr_y + value))
 
 print(dp[-1][-1])
