fad8472ca477ca998a3df339295a9b2e43f99e6e
sodabeans <44111674+sodabeans@users.noreply.github.com>
2022-12-30 16:14:16 +0900

    backtracking and bfs

--- Baekjoon/(Brute Force, Backtracking) 18428.py
+++ Baekjoon/(Brute Force, Backtracking) 18428.py
@@ -2,37 +2,44 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-hall = list(list(input().rstrip().split()) for _ in range(N))
-directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-students = []
+hall = []
 teachers = []
-obstacles = 3
-
 for i in range(N):
+    hall.append(list(input().rstrip().split()))
     for j in range(N):
-        if hall[i][j] == 'S':
-            students.append((i, j))
-        elif hall[i][j] == 'T':
+        if hall[i][j] == 'T':
             teachers.append((i, j))
 
-if not students or not teachers:
-    print("YES")
-    exit()
 
-for x_t, y_t in teachers:
-    for idx in range(4):
-        for j in range(1, N):
-            new_x = x_t + (directions[idx][0] * j)
-            new_y = y_t + (directions[idx][1] * j)
-            if 0 <= new_x < N and 0 <= new_y < N:
-                if hall[new_x][new_y] == 'T' or hall[new_x][new_y] == 'O':
+def search():
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    for r, c in teachers:
+        for idx in range(4):
+            new_r, new_c = r, c
+            while 0 <= new_r < N and 0 <= new_c < N:
+                if hall[new_r][new_c] == 'O':
                     break
-                elif hall[new_x][new_y] == 'S':
-                    if obstacles:
-                        obstacles -= 1
-                        hall[x_t + directions[idx][0]][y_t + directions[idx][1]] = 'O'
-                    else:
-                        print("NO")
-                        exit()
+                elif hall[new_r][new_c] == 'S':
+                    return False
+                new_r += directions[idx][0]
+                new_c += directions[idx][1]
+    return True
 
-print("YES")
+
+def selection(obstacle_cnt):
+    if obstacle_cnt == 3:
+        if search():
+            print("YES")
+            exit()
+            return
+    else:
+        for i in range(N):
+            for j in range(N):
+                if hall[i][j] == 'X':
+                    hall[i][j] = 'O'
+                    selection(obstacle_cnt + 1)
+                    hall[i][j] = 'X'
+
+
+selection(0)
+print("NO")
