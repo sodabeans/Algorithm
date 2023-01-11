c340a380354ca416c94eee3ff80cb469dabd1798
sodabeans <44111674+sodabeans@users.noreply.github.com>
2023-01-11 18:47:23 +0900

    dfs with dynamic programming

--- Baekjoon/Graph Search (BFS, DFS)/(DFS) 1520.py
+++ Baekjoon/Graph Search (BFS, DFS)/(DFS) 1520.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-sys.setrecursionlimit(10000)
-
 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 M, N = map(int, input().split())
 input_map = [list(map(int, input().split())) for _ in range(M)]
-visited = [[0 for _ in range(N)] for _ in range(M)]
-answer = 0
+visited = [[-1 for _ in range(N)] for _ in range(M)]
 
 
 def search(x, y):
-    global answer
+    if x == M - 1 and y == N - 1:
+        return 1
 
-    for idx in range(len(directions)):
-        dx, dy = directions[idx]
-        new_x = x + dx
-        new_y = y + dy
-        if 0 <= new_x < M and 0 <= new_y < N and visited[new_x][new_y] == 0:
-            if input_map[new_x][new_y] < input_map[x][y]:
-                if new_x == M - 1 and new_y == N - 1:
-                    answer += 1
-                    continue
-                visited[new_x][new_y] = 1
-                search(new_x, new_y)
-                visited[new_x][new_y] = 0
+    if visited[x][y] == -1:
+        visited[x][y] = 0
+        for idx in range(len(directions)):
+            dx, dy = directions[idx]
+            new_x = x + dx
+            new_y = y + dy
+            if 0 <= new_x < M and 0 <= new_y < N:
+                if input_map[new_x][new_y] < input_map[x][y]:
+                    visited[x][y] += search(new_x, new_y)
+    return visited[x][y]
 
 
-visited[0][0] = 1
-search(0, 0)
-print(answer)
+print(search(0, 0))
