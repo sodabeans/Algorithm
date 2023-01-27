649114dc33741c096e57eea05d314348a8958fc0
sodabeans <44111674+sodabeans@users.noreply.github.com>
2023-01-27 23:20:28 +0900

    DS1 day 10 Tree traversal

---
+++ LeetCode/Data Structure I/Day 10-3 145.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        answer = []
+        def traversal(node):
+            if node:
+                traversal(node.left)
+                traversal(node.right)
+                answer.append(node.val)
+        traversal(root)
+        return answer
