bf6574bc203513ab20c2381db48556b54886b24d
sodabeans <44111674+sodabeans@users.noreply.github.com>
2023-02-10 23:44:40 +0900

    LC1 Day 6

---
+++ LeetCode/LeetCode 75 Level 1/Day 6-2 102.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        answer = []
+        if not root:
+            return answer
+        queue = []
+        queue.append(root)
+        while queue:
+            level = len(queue)
+            curr_level = []
+            for _ in range(level):
+                curr = queue.pop(0)
+                curr_level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            answer.append(curr_level)
+        return answer
